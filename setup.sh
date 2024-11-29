61ff1adbd3885420292650569369161485ba1a4d
Minsub Kim <rapidfsub@gmail.com>
2024-11-30 08:53:45 +0900

    install vscode extensions in setup.sh

--- setup.sh
+++ setup.sh
@@ -33,4 +33,9 @@ if ! command -v brew &>/dev/null; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-brew bundle --cleanup --zap
+
+zsh -c '
+  task chezmoi:apply
+  . ~/.zprofile
+  task brew:bundle
+  task vscode:sync-extensions'
